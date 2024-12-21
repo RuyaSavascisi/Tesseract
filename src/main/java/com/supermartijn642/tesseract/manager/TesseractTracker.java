4668f3ede25d6300006187fcd700a9336bdf1d8a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-12-21 16:44:55 +0100

    Clear leftover tesseract references when joining a world

--- src/main/java/com/supermartijn642/tesseract/manager/TesseractTracker.java
+++ src/main/java/com/supermartijn642/tesseract/manager/TesseractTracker.java
@@ -115,9 +115,15 @@ public class TesseractTracker {
         }
         // Handle dirty references
         if(!SERVER.dirtyReferences.isEmpty()){
-            Tesseract.CHANNEL.sendToAllPlayers(new PacketAddTesseractReferences(new ArrayList<>(SERVER.dirtyReferences)));
+            Tesseract.CHANNEL.sendToAllPlayers(new PacketAddTesseractReferences(new ArrayList<>(SERVER.dirtyReferences), false));
             SERVER.dirtyReferences.clear();
         }
+    }
+
+    public void clear(){
+        if(this == SERVER)
+            throw new IllegalStateException("Server references cannot be cleared!");
+        this.tesseracts.clear();
     }
 
     public CompoundNBT writeKey(TesseractReference reference){
@@ -205,6 +211,6 @@ public class TesseractTracker {
 
     public static void sendReferences(PlayerEntity player){
         Collection<TesseractReference> references = SERVER.tesseracts.values().stream().map(Map::values).flatMap(Collection::stream).collect(Collectors.toList());
-        Tesseract.CHANNEL.sendToPlayer(player, new PacketAddTesseractReferences(references));
+        Tesseract.CHANNEL.sendToPlayer(player, new PacketAddTesseractReferences(references, true));
     }
 }
